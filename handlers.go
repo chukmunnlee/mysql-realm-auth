166f01000ff28c2d8eaab565003a40f8ac20e336
Chuk <chukmunnlee@gmail.com>
2021-05-27 12:16:05 +0800

    ADDED: authenticate

--- handlers.go
+++ handlers.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -15,6 +17,8 @@ const (
 	FORM_URL_ENCODED  = "application/x-www-form-urlencoded"
 	JSON              = "application/json"
 	HTML              = "text/html"
+	FIELD_USERNAME    = "username"
+	FIELD_PASSWORD    = "password"
 )
 
 func index(c *gin.Context) {
@@ -25,12 +29,12 @@ func index(c *gin.Context) {
 func healthz(authDB AuthDB) func(*gin.Context) {
 	return func(c *gin.Context) {
 		if err := authDB.Ping(); nil != err {
-			c.JSON(500, gin.H{
+			c.JSON(http.StatusInternalServerError, gin.H{
 				"message": err.Error(),
 			})
 			return
 		}
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"message": fmt.Sprintf("OK: %s", time.Now().Format(time.RFC850)),
 		})
 	}
@@ -38,5 +42,36 @@ func healthz(authDB AuthDB) func(*gin.Context) {
 
 func authenticate(authDB AuthDB) func(*gin.Context) {
 	return func(c *gin.Context) {
+		var user User
+
+		contentType := strings.ToLower(c.Request.Header.Get(CONTENT_TYPE))
+
+		if strings.HasPrefix(contentType, JSON) {
+			user = User{}
+			if err := c.BindJSON(&user); nil != err {
+				c.JSON(http.StatusNotAcceptable, gin.H{
+					"message": fmt.Sprintf("Cannot read credentials: %s", err.Error()),
+				})
+				return
+			}
+		} else if strings.HasPrefix(contentType, FORM_URL_ENCODED) {
+			user = User{
+				Username: c.PostForm(FIELD_USERNAME),
+				Password: c.PostForm(FIELD_PASSWORD),
+			}
+		} else {
+			c.JSON(http.StatusNotAcceptable, gin.H{
+				"message": fmt.Sprintf("Authenticate only supports %s and %s", JSON, FORM_URL_ENCODED),
+			})
+			return
+		}
+
+		log.Printf("username: %s, password: %s\n", user.Username, user.Password)
+
+		//result, err := authDB.Validate("abc", "xyz")
+
+		c.JSON(http.StatusOK, gin.H{
+			"user": user,
+		})
 	}
 }
